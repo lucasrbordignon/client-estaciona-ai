2248ca954e706471f4b51e1d2e45c105aff1f937
lucasrbordigon <lucas.r.bordignon@gmail.com>
2024-04-02 22:14:55 -0300

    mudando interface

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { HiOutlineCheckCircle, HiPlus, HiX } from "react-icons/hi";
 
 export default function Home() {
   const cars = [
@@ -35,25 +36,29 @@ export default function Home() {
   ]
 
   const [selectedCar, setSelectedCar] = useState(null)
+  const [showPopup, setShowPopup] = useState(false)
 
-  const handleCheckboxChange = (carId) => {
-    if ( selectedCar === carId ) {
-      setSelectedCar(null)
-    } else {
-      setSelectedCar(carId)
-    }
+  const handleFinishClick = (carId) => {
+    setSelectedCar(carId);
+    setShowPopup(true);
+  }
+
+  const handleClosePopup = () => {
+    setSelectedCar(null);
+    setShowPopup(false);
   }
 
   return (
     <div className="h-full">
       <div className="h-full w-full">
         <div className="max-w-6xl text-center py-8 m-auto">
-          <h1 className="text-4xl font-bold m-auto text-zinc-800">Estaciona Aí 🚘</h1>
+          <h1 className="text-4xl font-bold m-auto text-zinc-700">Estaciona Aí 🚘</h1>
         </div>
 
-        <div className="max-w-6xl text-center pb-8 m-auto w-full flex">
-          <button className="bg-orange-400 rounded-xl py-3 px-4 text-zinc-800 font-semibold border border-zinc-300 hover:border-zinc-400 hover:cursor-pointer hover:bg-orange-400 hover:shadow-inner">
-            Estacionar +
+        <div className="max-w-6xl text-center pb-4 m-auto w-full flex">
+          <button className="flex items-center gap-2 px-4 py-2 bg-green-500 font-semibold text-zinc-900 border border-zinc-400 rounded-lg hover:border-green-900 hover:cursor-pointer hover:bg-green-400 hover:shadow">
+            Estacionar
+            <HiPlus/>
           </button>
         </div>
 
@@ -61,24 +66,21 @@ export default function Home() {
           <table className="table-auto w-full overflow-x-scroll">
             <thead>
               <tr>
-                <th className="px-4 py-3 bg-orange-400 rounded-l-xl"></th>
-                <th className="px-4 py-3 bg-orange-400 text-left">Número</th>
-                <th className="px-4 py-3 bg-orange-400 text-left">Carro</th>
-                <th className="px-4 py-3 bg-orange-400 text-left">Placa</th>
-                <th className="px-4 py-3 bg-orange-400 text-left">Nome do Cliente</th>
-                <th className="px-4 py-3 bg-orange-400 text-left rounded-r-xl">Telefone do Cliente</th>
+                <th className="px-4 py-2 bg-zinc-400 rounded-l-lg"></th>
+                <th className="px-4 py-2 bg-zinc-400 text-left">Número</th>
+                <th className="px-4 py-2 bg-zinc-400 text-left">Carro</th>
+                <th className="px-4 py-2 bg-zinc-400 text-left">Placa</th>
+                <th className="px-4 py-2 bg-zinc-400 text-left">Nome do Cliente</th>
+                <th className="px-4 py-2 bg-zinc-400 text-left rounded-r-lg">Telefone do Cliente</th>
               </tr>
             </thead>
             <tbody>
               {cars.map(car => (
-                <tr key={car.id} className={selectedCar === car.id ? "bg-orange-200" : ""}>
-                  <td className="border-b border-zinc-400 px-4 py-2 rounded-l-xl text-center">
-                    <input
-                      type="checkbox"
-                      checked={selectedCar === car.id}
-                      onChange={() => handleCheckboxChange(car.id)}
-                      className="form-checkbox w-4"
-                    />
+                <tr key={car.id}>
+                  <td className="border-b border-zinc-400 px-4 py-2 rounded-l-lg text-center">
+                    <button onClick={() => handleFinishClick(car.id)}>
+                      <HiOutlineCheckCircle className="text-green-600 cursor-pointer hover:text-green-400" />
+                    </button>
                   </td>
                   <td className="border-b border-zinc-400 px-4 py-2">{car.numero}</td>
                   <td className="border-b border-zinc-400 px-4 py-2">{car.modelo}</td>
@@ -89,6 +91,18 @@ export default function Home() {
               ))}
             </tbody>
           </table>
+
+          {
+            showPopup && 
+          (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+              <div className="bg-white p-8 rounded-lg relative">
+                <button onClick={handleClosePopup} className="absolute right-2 top-2"><HiX className="text-red-500 hover:text-red-600"/></button>
+                <p className="mb-4">Deseja realmente finalizar esta vaga?</p>                                  
+                <button onClick={handleClosePopup} className="px-4 py-2 bg-green-500 font-semibold text-zinc-900 border border-zinc-400 rounded-lg hover:border-green-900 hover:cursor-pointer hover:bg-green-400 hover:shadow">Finalizar</button>
+              </div>
+            </div>
+          )}
         </div>
       </div> 
     </div>
